dee34b4fda465ef7cb39c2e3bf99cdbf7e19cfba
root <root@iobroker-dev.stadtvila>
2022-06-06 21:37:34 +0000

    Init Commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'pool-manager adapter settings': {
+		'en': 'Adapter settings for pool-manager',
+		'de': 'Adaptereinstellungen für pool-manager',
+		'ru': 'Настройки адаптера для pool-manager',
+		'pt': 'Configurações do adaptador para pool-manager',
+		'nl': 'Adapterinstellingen voor pool-manager',
+		'fr': "Paramètres d'adaptateur pour pool-manager",
+		'it': "Impostazioni dell'adattatore per pool-manager",
+		'es': 'Ajustes del adaptador para pool-manager',
+		'pl': 'Ustawienia adaptera dla pool-manager',
+		'zh-cn': 'pool-manager的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'вариант 2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
